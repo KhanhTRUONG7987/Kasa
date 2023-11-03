34619618cdd8fcd9fd21957c90f364d95c603674
khanh.truong <abby79truong@gmail.com>
2023-11-03 23:26:27 +0100

    close project

--- my-kasa-app/src/components/Card.jsx
+++ my-kasa-app/src/components/Card.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import '../styles/Card.css';
+import React from "react";
+import "../styles/Card.css";
 
 const Card = ({ image, title }) => {
   return (
     <div className="card">
-      <div className="card-overlay">
-        <p className="card-title">{title}</p>
+      <div className="card-component">
         <img className="card-image" src={image} alt={title} />
+        <div className="card-overlay"></div>
+        <p className="card-title">{title}</p>
       </div>
     </div>
   );
-}
+};
 
 export default Card;
